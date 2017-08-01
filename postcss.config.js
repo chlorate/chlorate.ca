a408ff7fe8cfdb48d3e3319341a72297e48dc576
Chlorate <me@chlorate.ca>
2017-08-01 18:58:13 -0300

    Fix uncss picking up dynamic script tag

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,11 @@
 module.exports = {
 	plugins: {
 		"postcss-uncss": {
-			html: ["mm10rng/*.tsx", "site/**/*.html"],
+			html: [
+				"mm10rng/*.tsx",
+				"site/content/**/*.html",
+				"site/layouts/{_default,shortcodes}/**/*.html",
+			],
 			ignore: [/\.page-.*/],
 		},
 	},
