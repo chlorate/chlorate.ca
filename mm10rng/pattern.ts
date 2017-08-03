1617d5155782c801141128186f9b8b45c4bd083a
Chlorate <me@chlorate.ca>
2017-08-02 21:08:47 -0300

    Simplify Wily 2 patterns
    Can't really reuse. Solar goes to three attacks instead of two.

--- mm10rng/pattern.ts
+++ mm10rng/pattern.ts
@@ -6,38 +6,27 @@ export interface Pattern {
 	grade: Grade;
 }
 
-enum SuzakFenixPatternName {
-	hh = "horizontal, horizontal",
-	hv = "horizontal, vertical",
-	vh = "vertical, horizontal",
-}
-
-const suzakFenixWily2Grades = {
-	[SuzakFenixPatternName.hh]: grades.worst,
-	[SuzakFenixPatternName.hv]: grades.best,
-	[SuzakFenixPatternName.vh]: grades.good,
-};
+const patternBit = 0x10000;
+const doorToAttack = 221;
+const horizToAttack = 196;
 
 export function suzakFenixWily2(seed: number): Pattern {
-	let name = suzakFenix(seed);
-	return {
-		grade: suzakFenixWily2Grades[name],
-		name: name,
-	};
-}
-
-function suzakFenix(seed: number): string {
-	const patternBit = 0x10000;
-	const doorToAttack = 221;
-	const horizToAttack = 196;
-
 	seed = random(seed, doorToAttack);
 	if (seed & patternBit) {
 		seed = random(seed, horizToAttack);
 		if (seed & patternBit) {
-			return SuzakFenixPatternName.hh;
+			return {
+				name: "horizontal, horizontal",
+				grade: grades.worst,
+			};
 		}
-		return SuzakFenixPatternName.hv;
+		return {
+			name: "horizontal, vertical",
+			grade: grades.best,
+		};
 	}
-	return SuzakFenixPatternName.vh;
+	return {
+		name: "vertical, horizontal",
+		grade: grades.good,
+	};
 }
