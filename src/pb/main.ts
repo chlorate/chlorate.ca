e9273ea97ad487697d68a142553204154d9d08b5
Chlorate <me@chlorate.ca>
2019-08-24 16:26:47 -0300

    Fix yargs type errors

--- src/pb/main.ts
+++ src/pb/main.ts
@@ -3,30 +3,39 @@ import * as https from "https";
 import * as yargs from "yargs";
 import { CsvParser } from "./csv";
 
-const args = yargs.command("* <url> <jsonPath>", "parses a CSV file", yargs =>
-  yargs
-    .positional("url", { describe: "URL for a CSV file", type: "string" })
-    .positional("jsonPath", {
-      describe: "path to output JSON file",
-      type: "string"
-    })
-).argv;
+yargs.command(
+  "$0 <url> <jsonPath>",
+  "parses a CSV file",
+  yargs =>
+    yargs
+      .positional("url", {
+        describe: "URL for a CSV file",
+        type: "string",
+        demandOption: "true"
+      })
+      .positional("jsonPath", {
+        describe: "path to output JSON file",
+        type: "string",
+        demandOption: "true"
+      }),
+  args => {
+    https.get(args.url, response => {
+      const { statusCode } = response;
+      if (statusCode !== 200) {
+        throw new Error(`request failed with status: {statusCode}`);
+      }
 
-https.get(args.url, response => {
-  const { statusCode } = response;
-  if (statusCode !== 200) {
-    throw new Error(`request failed with status: {statusCode}`);
+      let body = "";
+      response.on("data", chunk => (body += chunk));
+      response.on("end", () => handleRequestSuccess(body));
+    });
+
+    async function handleRequestSuccess(csv: string): Promise<void> {
+      const parser = new CsvParser();
+      await parser.parse(csv);
+
+      const json = JSON.stringify({ series: parser.series });
+      fs.writeFileSync(args.jsonPath, json);
+    }
   }
-
-  let body = "";
-  response.on("data", chunk => (body += chunk));
-  response.on("end", () => handleRequestSuccess(body));
-});
-
-async function handleRequestSuccess(csv: string): Promise<void> {
-  const parser = new CsvParser();
-  await parser.parse(csv);
-
-  const json = JSON.stringify({ series: parser.series });
-  fs.writeFileSync(args.jsonPath, json);
-}
+).argv;
